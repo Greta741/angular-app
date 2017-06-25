da3f95aabce70228013f538f29aed0717a89324b
Greta Žebrauskaitė <greta.zebrauskaite@gmail.com>
2017-06-25 14:42:44 +0300

    Add empty third view files

---
+++ angular-app/src/app/map-information/map-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-information',
+  templateUrl: './map-information.component.html',
+  styleUrls: ['./map-information.component.css']
+})
+export class MapInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
